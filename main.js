b7a0b0309d157f56cc2fa3f76c069b0064ba720d
Diego Espinoza <diego.espinoza1501@alumnos.ubiobio.cl>
2023-10-11 22:42:36 -0300

    +Filtro atracciones

---
+++ main.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const filterInput = document.getElementById('filterInput');
+    const cardContainer = document.getElementById('cardContainer');
+    const cards = cardContainer.querySelectorAll('.col');
+
+
+    function filterCards() {
+        const filterText = filterInput.value.toLowerCase();
+
+        cards.forEach((card) => {
+            const cardTitle = card.querySelector('.card-title').textContent.toLowerCase();
+
+            if (cardTitle.includes(filterText)) {
+                card.style.display = 'block';
+            } else {
+                card.style.display = 'none';
+            }
+        });
+    }
+    filterInput.addEventListener('input', filterCards);
+    filterCards();
+});
